278c3ba712a50d79658744ebd69602b9f8811a79
Jesse Sandvik <sandvikjesse@gmail.com>
2021-10-21 22:02:55 -0400

    database connected, first post added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Layout from '../components/layout';
+import prisma from '../lib/prisma';
 
-export default function Home() {
+export const getStaticProps: GetStaticProps = async () => {
+  const feed = await prisma.post.findMany();
+
+  return { props: { feed } };
+};
+
+export default function Home({ feed }) {
   return (
     <Layout>
       <Head><title>WhiteRabbt</title></Head>
       <section>
         <h2>Welcome</h2>
+        {JSON.stringify(feed)}
       </section>
     </Layout>
   );
